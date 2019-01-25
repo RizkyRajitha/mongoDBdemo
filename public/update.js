8fef3e23361b2b3fdebc316a1499464534b73cda
RizkyRajitha <rajithagunathilake@gmail.com>
2019-01-26 00:23:33 +0530

    modified update.js typo

--- public/update.js
+++ public/update.js
@@ -1,36 +1,39 @@
-console.log('in uodate.js')
+console.log("in uodate.js");
 
 const api = "/updateUsers";
 
-
-let upname1 = document.querySelector('#upname')
-let btn = document.querySelector('#btn1')
+let upname1 = document.querySelector("#upname");
+let btn = document.querySelector("#btn1");
 const txtfeild1 = document.querySelector("#name1");
 const txtfeild2 = document.querySelector("#pass1");
 const txtfeild3 = document.querySelector("#age1");
 const txtfeild4 = document.querySelector("#hometown1");
 const txtfeild5 = document.querySelector("#city1");
 
-btn.addEventListener('click',e=>{
-    e.preventDefault();
-    let name = txtfeild1.value;
-    let pass = txtfeild2.value;
-    let age = txtfeild3.value;
-    let hometown = txtfeild4.value;
-    let city = txtfeild5.value;
-    let upname = upname1.value;
+btn.addEventListener("click", e => {
+  e.preventDefault();
+  let name = txtfeild1.value;
+  let pass = txtfeild2.value;
+  let age = txtfeild3.value;
+  let hometown = txtfeild4.value;
+  let city = txtfeild5.value;
+  let upname = upname1.value;
 
-    let message = { upname:upname, name:name , pass:pass , age:age , hometown:hometown}
-    let data = JSON.stringify(message);
-    //data = 'enwada'
-    console.log(data)
+  let message = {
+    upname: upname,
+    name: name,
+    pass: pass,
+    age: age,
+    hometown: hometown,
+    city: city
+  };
+  let data = JSON.stringify(message);
+  //data = 'enwada'
+  console.log(data);
 
-    fetch(api,{
-        method:'POST',
-        body:data,
-        headers:{"Content-Type": "application/json"}
-    }).then()
-    
-
-})
-
+  fetch(api, {
+    method: "POST",
+    body: data,
+    headers: { "Content-Type": "application/json" }
+  }).then();
+});
