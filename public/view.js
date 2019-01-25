11b261de8653abb08e482ab51fa9f92fd6954120
RizkyRajitha <rajithagunathilake@gmail.com>
2019-01-26 00:16:04 +0530

    aded cool metirialized css styles

--- public/view.js
+++ public/view.js
@@ -23,8 +23,29 @@ btn.addEventListener("click", e => {
 
     r.forEach(element => {
 
-      divres.innerHTML += "<p> name -  "+element.name +"<br> passowrd - "+element.password+"<br> age - "+element.age+"<br> hometown - "+element.hometown+"<br> city - "+element.city+"</p>"
+      //divres.innerHTML += "<p> name -  "+element.name +"<br> passowrd - "+element.password+"<br> age - "+element.age+"<br> hometown - "+element.hometown+"<br> city - "+element.city+"</p>"
+      divres.innerHTML+=`\
+      \
+      <div class="row">\
+      <div class="col s12 ">\
+        <div class="card grey darken-4">\
+          <div class="card-content white-text">\
+            <span class="card-title">${element.name}</span>\
+            <p id="p1">${element.name}</p>
+            <p id="p2">${element.password}</p>
+            <p id="p3">${element.age}</p>
+            <p id="p4">${element.hometown}</p>
+            <p id="p5">${element.city}</p>
+          </div>
+        </div>
+      </div>
+    </div>
       
+      
+      
+      `
+
+
     });
   
   
