a84d5ececa9e37d03b8db6fbf212321dc4a6b0d8
RizkyRajitha <rajithagunathilake@gmail.com>
2019-01-25 22:55:09 +0530

    added config vars

--- db/people.js
+++ db/people.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 //mongoose.connect("mongodb://127.0.0.1:27017/WebApp");
-const mongodbAPI = 'mongodb://rizkyrajitha:fariz123123@ds043210.mlab.com:43210/demo' || 'mongodb://127.0.0.1:27017/WebApp';
-mongoose.connect(mongodbAPI)
+//const mongodbAPI = 'mongodb://rizkyrajitha:fariz123123@ds043210.mlab.com:43210/demo' || 'mongodb://127.0.0.1:27017/WebApp';
+const mongodbAPI = process.env.MONGOAPI || "mongodb://127.0.0.1:27017/WebApp";
+
+mongoose.connect(mongodbAPI);
 
 var Schema = mongoose.Schema;
 
@@ -12,17 +14,23 @@ var userSchema = new Schema({
     required: true,
     trim: true
   },
-  password: { 
+  password: {
     type: String,
-    required: true, 
-    trim: true },
-  age: { 
+    required: true,
+    trim: true
+  },
+  age: {
     type: Number,
-    trim: true },
-    hometown:{
-      type:String,
-      trim:true
-    }
+    trim: true
+  },
+  hometown: {
+    type: String,
+    trim: true
+  },
+  city: {
+    type: String,
+    trim: true
+  }
 });
 
 const User = mongoose.model("User", userSchema);
